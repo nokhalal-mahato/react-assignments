7887fe2d7f74321536d2dc2db15f5331beb9c2a2
nokhalal-nxtwave <nokhalal.mahato@nxtwave.co.in>
2022-10-14 19:10:57 +0530

    money tracker added

---
+++ src/components/MoneyTracker/moneydetail.js
@@ -0,0 +1,31 @@
+const moneydetail = [
+  {
+    id: "total",
+    balance: 0,
+    text: "Balance",
+    imageUrl:
+      "https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png",
+    bgcolor: "#ecfccb",
+    bordercolor: "#84cc16",
+  },
+  {
+    id: "income",
+    balance: 0,
+    text: "Income",
+    imageUrl:
+      "https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png",
+    bgcolor: "#cffafe",
+    bordercolor: "#06b6d4",
+  },
+  {
+    id: "expense",
+    balance: 0,
+    text: "Expense",
+    imageUrl:
+      "https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png",
+    bgcolor: "#ede9fe",
+    bordercolor: "#7c3aed",
+  },
+];
+
+export default moneydetail;
