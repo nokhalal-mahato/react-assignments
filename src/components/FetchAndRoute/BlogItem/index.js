5a636491dceacb9556207f392a7fc22cf7e23ac0
nokhalal-nxtwave <nokhalal.mahato@nxtwave.co.in>
2022-10-20 19:38:00 +0530

    resgistration-form

--- src/components/FetchAndRoute/BlogItem/index.js
+++ src/components/FetchAndRoute/BlogItem/index.js
@@ -1,27 +1,28 @@
 import { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import "./index.css";
 
 class BlogItem extends Component {
   render() {
+    const url = this.props.match.url;
     const { data } = this.props;
     return (
-      <Link to={`/blog/${data.id}`} className="bloglist-item">
-          <div className="bloglist-item-left-section">
-            <img src={data.imageUrl} />
-          </div>
-          <div className="bloglist-item-right-section">
-            <p className="bloglist-item-topic">{data.topic}</p>
-            <h3 className="bloglist-item-title">{data.title}</h3>
-            <div className="bloglist-item-author-container">
-              <div className="bloglist-item-author">
-                <img src={data.avatarUrl} />
-              </div>
-              <p className="bloglist-item-description">{data.author}</p>
+      <Link to={`${url}/${data.id}`} className="bloglist-item">
+        <div className="bloglist-item-left-section">
+          <img src={data.imageUrl} />
+        </div>
+        <div className="bloglist-item-right-section">
+          <p className="bloglist-item-topic">{data.topic}</p>
+          <h3 className="bloglist-item-title">{data.title}</h3>
+          <div className="bloglist-item-author-container">
+            <div className="bloglist-item-author">
+              <img src={data.avatarUrl} />
             </div>
+            <p className="bloglist-item-description">{data.author}</p>
           </div>
+        </div>
       </Link>
     );
   }
 }
-export default BlogItem;
+export default withRouter(BlogItem);
