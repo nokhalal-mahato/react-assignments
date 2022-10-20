5a98a02b2279cd272176a888c3e23dd701bf0555
nokhalal-nxtwave <nokhalal.mahato@nxtwave.co.in>
2022-10-20 10:21:24 +0530

    ipl-dashboard updated

--- src/components/IplDashboard/TeamMatch/index.js
+++ src/components/IplDashboard/TeamMatch/index.js
@@ -3,7 +3,16 @@ import "./index.css";
 import { TailSpin } from "react-loader-spinner";
 import LatestMatch from "../LatestMatch";
 import MatchCard from "../MatchCard";
-
+const bgcolor = {
+  KKR: "#5755a7",
+  RCB: "#a4261d",
+  KXP: "#d91c1f",
+  CSK: "#f7db00",
+  RR: "#da237b",
+  MI: "#13418b",
+  SH: "#f26d22",
+  DC: "#4f5db0",
+};
 class TeamMatch extends Component {
   state = { teamData: {}, isLoading: true, error: false };
   componentDidMount() {
@@ -37,8 +46,9 @@ class TeamMatch extends Component {
   render() {
     const { isLoading, error, teamData } = this.state;
     const {recentMatches}=teamData;
+    const color = bgcolor[this.props.match.params.id];
     return (
-      <div className="team-match-page">
+      <div className="team-match-page" style={{ backgroundImage : `linear-gradient(to bottom, ${color},black )`}}>
         {!isLoading && !error && (
           <div className="team-match-content">
             <div className="team-banner">
@@ -47,7 +57,9 @@ class TeamMatch extends Component {
             <h3>Latest Matchs</h3>
             <LatestMatch data={teamData.latestMatchDetails} />
             <div className="team-card-container">
-                {recentMatches.map((item)=><MatchCard key={item.id} data={item}/>)}
+              {recentMatches.map((item) => (
+                <MatchCard key={item.id} data={item} />
+              ))}
             </div>
           </div>
         )}
