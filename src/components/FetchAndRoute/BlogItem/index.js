729772227f36c8afa0a5bf87e1df9cc4684b7656
nokhalal-nxtwave <nokhalal.mahato@nxtwave.co.in>
2022-10-20 19:43:21 +0530

    resgistration-form

--- src/components/FetchAndRoute/BlogItem/index.js
+++ src/components/FetchAndRoute/BlogItem/index.js
@@ -7,20 +7,20 @@ class BlogItem extends Component {
     const url = this.props.match.url;
     const { data } = this.props;
     return (
-      <Link to={`${url}/${data.id}`} className="bloglist-item">
-        <div className="bloglist-item-left-section">
-          <img src={data.imageUrl} />
-        </div>
-        <div className="bloglist-item-right-section">
-          <p className="bloglist-item-topic">{data.topic}</p>
-          <h3 className="bloglist-item-title">{data.title}</h3>
-          <div className="bloglist-item-author-container">
-            <div className="bloglist-item-author">
-              <img src={data.avatarUrl} />
-            </div>
-            <p className="bloglist-item-description">{data.author}</p>
+      <Link to={`${url}/blog/${data.id}`} className="bloglist-item">
+          <div className="bloglist-item-left-section">
+            <img src={data.imageUrl} />
           </div>
-        </div>
+          <div className="bloglist-item-right-section">
+            <p className="bloglist-item-topic">{data.topic}</p>
+            <h3 className="bloglist-item-title">{data.title}</h3>
+            <div className="bloglist-item-author-container">
+              <div className="bloglist-item-author">
+                <img src={data.avatarUrl} />
+              </div>
+              <p className="bloglist-item-description">{data.author}</p>
+            </div>
+          </div>
       </Link>
     );
   }
